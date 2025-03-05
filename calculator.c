935b17eaf774ba450f62f546c0399f139c03a580
Akshatsah04 <iamakshatsah@gmail.com>
2025-03-05 16:28:09 +0530

    class work

---
+++ calculator.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+
+    int a , b ;
+    int opre;
+
+    printf("enter two number\n");
+    scanf("%d%d", &a ,&b);
+
+    printf("1.addition\n2.substraction\n3.multiply\n4.divide\n");
+    scanf("%d",&opre);
+
+    switch (opre)
+    {
+    case 1:
+        printf("%d",a+b);
+        break;
+    case 2:
+        printf("%d",a-b);
+        break;
+    case 3:
+        printf("%d",a*b);
+        break;
+    case 4:
+        printf("%d",a/b);
+        break;
+    
+    default:
+    printf("invalid input");
+        break;
+    }
+
+    return 0;
+
+
+}
---
+++ electricbill.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int call , amount=100;
+
+    printf("enter the no of call"):
+    scanf("%d",&call);
+
+    switch (call/50)
+    {
+    case constant expression:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+
+}
