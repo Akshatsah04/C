b5610cf3ed8b49cb1aa1a9cdea0817e00ccc05e7
Akshatsah04 <iamakshatsah@gmail.com>
2025-03-11 12:54:11 +0530

    lab 4 q

--- lab4/electricbill.c
+++ lab4/electricbill.c
@@ -1,20 +1,29 @@
 #include<stdio.h>
 
 int main(){
-    int call , amount=100;
+    int unit , amount=0;
 
-    printf("enter the no of call"):
-    scanf("%d",&call);
+    printf("enter the amount of units used\n");
+    scanf("%d",&unit);
 
-    switch (call/50)
+    switch (unit)
     {
-    case constant expression:
-        /* code */
+    case 1 ... 100:
+        printf("amount=%d",amount);
+        break;
+    case 101 ... 200:
+        printf("amount=%.2f",(unit-100)*1.5);
+        break;
+    case 201 ... 400:
+        printf("amount=%.2f",150+(unit-200)*2.5);
         break;
     
     default:
+        printf("amount=%.2f",150+500+(unit-200)*3.5);
         break;
     }
+
+    return 0;
 
 
 }
