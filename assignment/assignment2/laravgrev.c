71479559ef13fd02805bc621f46b26339f8b219a
Akshatsah04 <iamakshatsah@gmail.com>
2025-04-14 17:27:50 +0530

    assignment2

--- assignment/assignment2/laravgrev.c
+++ assignment/assignment2/laravgrev.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int n;
+    int n , max , min , sum=0 , avg ,t;
     printf("enter the size of array: ");
     scanf("%d",&n);
     int arr[n];
@@ -10,7 +10,40 @@ int main(){
     {
         scanf("%d",&arr[i]);
     }
+    max=arr[0];
+    min=arr[0];
 
-     0
+    for (int i = 0; i < n; i++)
+    {
+        if(max<arr[i]){
+            max=arr[i];
+        }
+        if (min>arr[i])
+        {
+            min=arr[i];
+        }
+
+        sum+=arr[i];  
+    }
+    for (int i = 0; i < n/2; i++)
+    {
+        t=arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=t;
+    }
+    printf("largent element in the given array is %d\n" , max);
+    printf("smallest element in the given array is %d\n" , min);
+    printf("sum of the element in the given array is %d\n" , sum);
+    printf("average of the element in the given array is %0.2f\n" , (sum*1.0)/n);
+    printf("reversed array\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+    
+    
+    
+
     
 }
