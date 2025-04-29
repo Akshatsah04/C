f8c839b88841990e7846cac9f6cb47a4ff481ecb
Akshatsah04 <iamakshatsah@gmail.com>
2025-04-29 16:21:38 +0530

    lab10 q3

---
+++ lab10/3-gcd.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int gcd(int a , int b);
+
+int main(){
+    int a,b;
+    printf("enter two number\n");
+    scanf("%d%d", &a , &b);
+
+    if (a>b)
+    {
+        printf("gcd of these two number is %d" , gcd(a ,b));
+    }else{
+        printf("gcd of these two number is %d" , gcd(b ,a));
+    }
+    
+    return 0;
+
+}
+
+int gcd(int a , int b){
+    int temp;
+
+    while (b!=0)
+    {
+        temp = b;
+        b=a%b;
+        a=temp;
+    }
+    
+    return temp;
+}
